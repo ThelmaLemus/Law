152d2e887d39c9d17b2ca4887b213bb94dd35a6e
ThelmaLemus <thelma.lemus@galileo.edu>
2019-11-25 20:17:40 -0600

    carta de poder en proceso

--- drywest/dashboard/examples/guardartemplates/guardar_cartadepoder.php
+++ drywest/dashboard/examples/guardartemplates/guardar_cartadepoder.php
@@ -10,11 +10,32 @@
     $DPI_testigo2 = $_POST['DPI_testigo2'];
     $fecha_caducidad = $_POST['fecha_caducidad'];
     $usuario = $_POST['usuario'];
+    $pid = $_POST['pid'];
+    $nombre_archivo = $_POST['nombre_archivo'];
 
     $link = pg_connect("host=localhost dbname=proyectoleyes user=postgres password=1998");
 
-    $query = "INSERT INTO carta_de_poder(fecha_emision, nombre_otorgante, nombre_apoderado, responsabilidades, dpi_otorgante, dpi_apoderado, dpi_testigo1, dpi_testigo2, fecha_caducidad, uid) 
-                VALUES ('$fecha_emision', '$nombre_otorgante', '$nombre_apoderado', '$responsabilidades', '$DPI_otorgante', '$DPI_apoderado', '$DPI_testigo1', '$DPI_testigo2', '$fecha_caducidad', $usuario)";
+    if($pid == 0)
+    {
+        $query = "INSERT INTO carta_de_poder(fecha_emision, nombre_otorgante, nombre_apoderado, responsabilidades, dpi_otorgante, dpi_apoderado, dpi_testigo1, dpi_testigo2, fecha_caducidad, uid, nombre_documento) 
+                VALUES ('$fecha_emision', '$nombre_otorgante', '$nombre_apoderado', '$responsabilidades', '$DPI_otorgante', '$DPI_apoderado', '$DPI_testigo1', '$DPI_testigo2', '$fecha_caducidad', $usuario, '$nombre_archivo')";
+    }
+    else
+    {
+        $query = "UPDATE carta_de_poder SET
+                    fecha_emision = '$fecha_emision',
+                    nombre_otorgante = '$nombre_otorgante',
+                    nombre_apoderado = '$nombre_apoderado',
+                    responsabilidades = '$responsabilidades',
+                    dpi_otorgante = '$DPI_otorgante',
+                    dpi_apoderado = '$DPI_apoderado',
+                    dpi_testigo1 = '$DPI_testigo1',
+                    dpi_testigo2 = '$DPI_testigo2',
+                    fecha_caducidad = '$fecha_caducidad',
+                    uid = $usuario,
+                    nombre_documento = '$nombre_archivo'
+                WHERE pid = $pid";
+    }
     
     $result = pg_query($link, $query);
     if($result){   
